75ee74440a934a188b4a60a8a2530527b0233b13
LeoLacana <wilson17414@gmail.com>
2021-04-16 19:16:54 -0300

    req 7

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 let elementButton = document.getElementById('criar-tarefa');
-let list = document.querySelector('#lista-tarefas');
+let list = document.querySelectorAll('#lista-tarefas');
 let elementInput = document.querySelector('#texto-tarefa');
 
-function addAssignment(value) {
+function addAssignment() {
   let newItem = document.createElement('li');
   newItem.innerText = elementInput.value;
-  list.appendChild(newItem);
+  list[0].appendChild(newItem);
   elementInput.value = '';
 }
 
+function colorAssigment(elementClicked) {
+  elementClicked.target.classList.add('color');
+}
+
 elementButton.addEventListener('click', addAssignment);
+
+const elementLi = document.getElementById('lista-tarefas');
+elementLi.addEventListener('click', colorAssigment);
