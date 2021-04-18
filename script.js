52e2e854d489e5d3cc9deaf209aae95f5945cb25
LeoLacana <wilson17414@gmail.com>
2021-04-17 21:47:14 -0300

    requisito

--- script.js
+++ script.js
@@ -1,23 +1,36 @@
-let elementButton = document.getElementById('criar-tarefa');
-let list = document.querySelectorAll('#lista-tarefas');
-let elementInput = document.querySelector('#texto-tarefa');
+const elementButton = document.getElementById('criar-tarefa');
+const list = document.querySelectorAll('#lista-tarefas');
+const elementInput = document.querySelector('#texto-tarefa');
 
 function addAssignment() {
-  let newItem = document.createElement('li');
+  const newItem = document.createElement('li');
   newItem.innerText = elementInput.value;
   list[0].appendChild(newItem);
   elementInput.value = '';
 }
 
+// Criando Tarefa
+elementButton.addEventListener('click', addAssignment);
+
 function colorAssigment(elementClicked) {
   const elementLi = document.querySelectorAll('li');
-  for (let index = 0; index < elementLi.length; index += 1){
+  for (let index = 0; index < elementLi.length; index += 1) {
     elementLi[index].classList.remove('color');
   }
   elementClicked.target.classList.add('color');
 }
 
-elementButton.addEventListener('click', addAssignment);
-
+// Selecionando Tarefa 
 const elementOl = document.getElementById('lista-tarefas');
 elementOl.addEventListener('click', colorAssigment);
+
+function lineThroughFunction (elementClicked){
+  if (elementClicked.target.classList.contains('completed')) {
+    elementClicked.target.classList.remove('completed');
+  } else {
+    elementClicked.target.classList.add('completed');
+  }
+}
+
+// Marca como completa a tarefa
+elementOl.addEventListener('dblclick', lineThroughFunction);
